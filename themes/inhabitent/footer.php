85d6692ef586d5cfdd232369f7c8d935c4160568
quinlan-alys <shannonqad@gmail.com>
2016-11-04 16:17:13 -0700

    first commit

---
+++ themes/inhabitent/footer.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * @package RED_Starter_Theme
+ */
+
+?>
+
+			</div><!-- #content -->
+
+			<footer id="colophon" class="site-footer" role="contentinfo">
+			<div class="contact">
+			<h1> Contact Info </h1>
+			<ul> <li><a href="#" class="email"> info@inhabitent.com </a></li>
+				<li> 778-456-7891</li>
+				<li> icons</li>
+				<div class="site-info">
+					<a href="<?php echo esc_url( 'https://wordpress.org/' ); ?>"><?php printf( esc_html( 'Proudly powered by %s' ), 'WordPress' ); ?></a>
+				</div><!-- .site-info -->
+			</footer><!-- #colophon -->
+		</div><!-- #page -->
+
+		<?php wp_footer(); ?>
+
+	</body>
+</html>
