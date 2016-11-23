1afd9433f785e66e16b7a2c5b911e59a21e75301
quinlan-alys <shannonqad@gmail.com>
2016-11-23 15:08:54 -0800

    fixing theme check required warnings

--- themes/inhabitent/functions.php
+++ themes/inhabitent/functions.php
@@ -88,6 +88,7 @@ function red_starter_scripts() {
 	wp_enqueue_script( 'red-starter-skip-link-focus-fix', get_template_directory_uri() . '/build/js/skip-link-focus-fix.min.js', array(), '20130115', true );
 	wp_enqueue_script('jquery');
 	wp_enqueue_script('nav-search', get_template_directory_uri() . '/js/scripts.js', array ('jquery'), false, true);
+
 	// //Delete after
 	// wp_enqueue_script('red_comments', get_template_directory_uri() . '/js/ajax-example.js', array('jquery'), false, true );
 
@@ -100,9 +101,10 @@ function red_starter_scripts() {
 	// ));
 
 
-	// if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
-	// 	wp_enqueue_script( 'comment-reply' );
-	// }
+	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
+		wp_enqueue_script( 'comment-reply' );
+	}
+
 }
 add_action( 'wp_enqueue_scripts', 'red_starter_scripts' );
 
